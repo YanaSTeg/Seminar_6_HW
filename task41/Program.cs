ee3a3486b63ca01a860ce15e1dd57e69346c61e4
Yana <sieghostienko@gmail.com>
2023-06-09 14:07:35 +0300

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+System.Console.WriteLine("Введите раззмер массива: ");
+int NewMass = Convert.ToInt32(Console.ReadLine());
+
+int PositiveNums()
+{
+    int Positive = 0;
+    for (int i = 0; i < NewMass; i++)
+    {
+        Console.WriteLine($"Введите число {i + 1}: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num > 0)
+            Positive++;
+    }
+    return Positive;
+}
+Console.WriteLine($"Положительных чисел: {PositiveNums()}");
